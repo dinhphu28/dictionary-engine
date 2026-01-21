34549067eef1a2a6ea26926ec9671dcb026d84ed
Dinh Phu Nguyen <dinhphu.nguyen.20@gmail.com>
2026-01-21 10:42:58 +0700

    Change startup process
    - Load config file
    - Load resources/ specified in config
    
    Signed-off-by: Dinh Phu Nguyen <dinhphu.nguyen.20@gmail.com>

--- engine.go
+++ engine.go
@@ -7,29 +7,17 @@ import (
 	"github.com/dinhphu28/dictionary/database"
 	"github.com/dinhphu28/dictionary/internal/config"
 	"github.com/dinhphu28/dictionary/internal/setup"
+	"github.com/dinhphu28/dictionary/internal/startup"
 )
 
 var (
+	globalConfig      config.GlobalConfig
 	dictionaries      []database.Dictionary
 	approximateLookup *ApproximateLookup
 )
 
 func StartEngine() {
-	paths := setup.DefaultPaths()
-	configPath := filepath.Join(paths.ConfigDir, "config.json")
-	resourcesPath := filepath.Join(paths.DataDir, "resources")
-
-	if err := config.LoadConfig(configPath); err != nil {
-		log.Fatal("failed to load config:", err)
-	}
-	globalConfig := config.GetGlobalConfig()
-
-	if err := database.LoadDictionaries(resourcesPath); err != nil {
-		log.Fatal("failed to load dictionaries:", err)
-	}
-	dictionaries = database.GetDictionaries()
-
-	log.Printf("Loaded %d dictionaries\n", len(dictionaries))
+	loadConfigAndData()
 
 	dictionaryLookup := NewDictionaryLookup(dictionaries, globalConfig)
 	approximateLookup = NewApproximateLookup(*dictionaryLookup)
@@ -53,4 +41,27 @@ func Lookup(q string) (LookupResultWithSuggestion, error) {
 		return LookupResultWithSuggestion{}, err
 	}
 	return result, nil
+}
+
+func loadConfigAndData() {
+	paths := setup.DefaultPaths()
+	configPath := filepath.Join(paths.ConfigDir, "config.json")
+
+	if err := config.LoadConfig(configPath); err != nil {
+		log.Fatal("failed to load config:", err)
+	}
+	globalConfig = config.GetGlobalConfig()
+
+	resourcesPath := globalConfig.DataDir
+	if !filepath.IsAbs(resourcesPath) {
+		resourcesPath = startup.ResolvePath(resourcesPath)
+	}
+	log.Printf("Resource Dir: %v\n", resourcesPath)
+
+	if err := database.LoadDictionaries(resourcesPath); err != nil {
+		log.Fatal("failed to load dictionaries:", err)
+	}
+	dictionaries = database.GetDictionaries()
+
+	log.Printf("Loaded %d dictionaries\n", len(dictionaries))
 }
--- internal/config/global_config.go
+++ internal/config/global_config.go
@@ -1,6 +1,6 @@
-// Package internal manifest structure to hold application metadata
 package config
 
 type GlobalConfig struct {
 	Priority []string `json:"priority"`
+	DataDir  string   `json:"data_dir"`
 }
